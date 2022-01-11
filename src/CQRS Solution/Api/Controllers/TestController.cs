7aa133a0b406a4de10d3aed825625e25cb8b3e3f
Gagarin23 <cuzitworks92@gmail.com>
2022-01-11 20:13:37 +0300

    Remove garbage

--- src/CQRS Solution/Api/Controllers/TestController.cs
+++ src/CQRS Solution/Api/Controllers/TestController.cs
@@ -4,21 +4,11 @@ using Application.Example.Streams;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Application.Common.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Api.Controllers
 {
     public class ExampleController : ApiController
     {
-        private readonly IDatabaseContext _context;
-
-        public ExampleController(IDatabaseContext context)
-        {
-            _context = context;
-        }
-        
         [HttpPost("ok")]
         public async Task<IActionResult> GetOkMessage(OkQuery request)
         {
@@ -36,17 +26,9 @@ namespace Api.Controllers
         }
 
         [HttpPost("publish")]
-        public IActionResult Post([FromBody] ExampleNotification notify)
+        public IActionResult Post(ExampleNotification notify)
         {
             Mediator.Publish(notify);
-
-            return Ok();
-        }
-
-        [HttpPost("test")]
-        public IActionResult test()
-        {
-            _context.Database.CanConnect();
 
             return Ok();
         }
