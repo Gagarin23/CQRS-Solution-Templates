0272c93aee8d8dfe3f8c97422a41899a173820bd
Gagarin23 <cuzitworks92@gmail.com>
2022-03-31 13:54:58 +0300

    remove src. Update visual studio template

--- src/CQRS Solution/Application/Example/Notifications/ExampleNotification.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-
-namespace Application.Example.Notifications
-{
-    public class ExampleNotification : INotification { }
-}
--- src/CQRS Solution/Application/Example/Queries/OkQuery.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-
-namespace Application.Example.Queries
-{
-    public class OkQuery : IRequest<string>
-    {
-        public string TestMsg { get; set; }
-    }
-}
